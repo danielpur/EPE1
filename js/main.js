9d2c44385dac0b73f6b59de2923ae6cd08c29238
Daniel Ubeda Reyes <uberedaniel@gmail.com>
2022-08-24 21:08:53 -0400

    Centro de ayuda - Contacto

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $('#slogan').fadeIn(2000);
+});
+
+$(document).ready(function() {
+    $('#destinos').fadeIn(2000);
+});
+
+$(document).ready(function() {
+    $('#centrodeayuda').fadeIn(2000);
+});
+
+
+$(document).ready(function() {
+    $('#img-contacto').fadeIn(2000);
+});
